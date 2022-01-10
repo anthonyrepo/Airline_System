004811b0067046ca30d7bed27b732eb61d2d603e
Anthony <78623358+anthonyrepo@users.noreply.github.com>
2022-01-09 21:17:52 -0500

    Add app helper classes

---
+++ src/AllReservationsViaSearchEngine.java
@@ -0,0 +1,22 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class AllReservationsViaSearchEngine extends JFrame {
+	
+	
+	
+	public AllReservationsViaSearchEngine() {
+		
+		super("Reservations Page");
+		
+		
+		
+		setSize(1200, 900);
+		setVisible(true);
+	}
+}
